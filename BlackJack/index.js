35c8ab2266cdfaf13d804ee17d7a9dde0480311d
Bartek Łuczak <b.luczak0@gmail.com>
2021-12-17 00:25:41 +0100

    Add newCard() and getRandomCard() functions

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,5 +1,6 @@
-let firstCard = 11
-let secondCard = 10
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -8,13 +9,26 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-function startGame() {
+function getRandomCard() {
+    randomCard = Math.floor(Math.random() * 13) + 1
+    return randomCard
+}
+
+function startGame(params) {
+    renderGame()
+}
+
+function renderGame() {
+
+    cardsEl.textContent = "Cards: "
+    for (i=0; i< cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum;
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!";
+        message = "You've got Blackjack!";
         hasBlackJack = true
     } else {
         message = "You're out of the game!";
@@ -23,3 +37,9 @@ function startGame() {
     messageEl.textContent = message
 }
 
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
